282fbbb0fb690c98d607a6f642c9ec9ac17ab821
Artem Averin <zalimhan.tekaev@simbirsoft.com>
2024-11-23 13:37:43 +0300

    a

--- backend/src/common/exceptionFilters/httpException.filter.ts
+++ backend/src/common/exceptionFilters/httpException.filter.ts
@@ -1,5 +1,5 @@
-import { ExceptionFilter, Catch, ArgumentsHost, HttpException } from '@nestjs/common';
-import { Request, Response } from 'express';
+import { ArgumentsHost, Catch, ExceptionFilter, HttpException } from '@nestjs/common';
+import { Response } from 'express';
 
 @Catch()
 export class HttpExceptionFilter implements ExceptionFilter {
--- backend/src/modules/articles/dto/article.dto.ts
+++ backend/src/modules/articles/dto/article.dto.ts
@@ -1,9 +1,9 @@
 import { ApiProperty } from '@nestjs/swagger';
-import { IsNotEmpty, IsString } from 'class-validator';
+import { IsNotEmpty, IsOptional, IsString } from 'class-validator';
 
 export class ArticleDto {
   @ApiProperty()
-  @IsNotEmpty()
+  @IsOptional()
   @IsString()
   title: string;
 
