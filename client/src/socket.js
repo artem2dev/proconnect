a445641159df020da3c6e7667440fc6adcc3feb0
Artem Averin <zalimhan.tekaev@simbirsoft.com>
2024-11-23 21:15:15 +0300

    d

--- client/src/socket.js
+++ client/src/socket.js
@@ -3,6 +3,6 @@ import { config } from './config/app.config';
 
 const URL = config.API_SOCKET;
 
-const socket = io(URL, { autoConnect: false, transports: ['websocket'] });
+const socket = io(URL, { autoConnect: false });
 
 export default socket;
